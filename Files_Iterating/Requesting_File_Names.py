8ccb5d0d1679367b8e4cfd794752cca6ad749c70
Kevin Russel <kzone159@gmail.com>
2025-03-08 12:39:36 -0500

    added our main and fixed up our threading

---
+++ Files_Iterating/Requesting_File_Names.py
@@ -0,0 +1,37 @@
+import os
+import time
+import threading
+
+
+
+## ok so what I can do right now is to make a class
+## this will get us the the current path of the file.
+# and I want to do is to to make a class and a main class..
+
+class Requesting_Files_Names:
+    ## we want to make a constructor that creates an object that has the directory namee and so forth.
+    
+    def __init__(self,currentFilePath,pictureDirectoryName):
+        self.currentFilePath = currentFilePath
+        self.pictureDirectoryName = pictureDirectoryName
+
+        self.filename = os.path.join(currentFilePath, 'test_images_8_bit')
+        print(self.filename)
+        self.checkThreading = False
+
+
+
+    def fun1(self):
+        print("Hello there!")
+        time.sleep(0.0025)
+
+
+    def run(self):
+        while self.checkThreading == False:
+            for name in os.listdir(self.filename):
+                with open(os.path.join(self.filename,name)) as f:
+                    threading.Thread(target=self.fun1).start()
+            self.checkThreading = True
+
+
+
